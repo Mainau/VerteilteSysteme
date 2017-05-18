850a5c7970a74b48d9b7731a71847b2147f0b195
Mainau <felix.thcilhcs@gmail.com>
2017-05-18 12:50:17 +0200

    Logging erstellt

--- src/aufgabe2a/WebServer.java
+++ src/aufgabe2a/WebServer.java
@@ -4,15 +4,26 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Created by felix on 18.05.17.
  */
 public class WebServer {
+    private int port;
+    private String filename;
+    private Logger logger;
 
+    public WebServer(int port, String filename) {
+        this.port = port;
+        this.filename = filename;
 
-    public static void main(String args[]) throws IOException {
-        ServerSocket server = new ServerSocket(8080);
+    }
+
+    public void listen() throws IOException {
+        ServerSocket server = new ServerSocket(port);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
             try (Socket socket = server.accept()) {
@@ -21,6 +32,37 @@ public class WebServer {
                 socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
             }
         }
+    }
+
+    public void setupLogging() {
+        logger = Logger.getLogger("MyLog");
+        FileHandler fh;
+
+        try {
+
+            // This block configure the logger with handler and formatter
+            fh = new FileHandler(filename);
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+
+            // the following statement is used to log any messages
+            logger.info("My first log");
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main(String args[]) throws IOException {
+        WebServer web = new WebServer(8080, "logfile");
+        web.listen();
+
+
     }
 
 
